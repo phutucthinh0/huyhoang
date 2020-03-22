e08a5892f9ea03c36d0ff4ff2150b054fc0cf701
phutucthinh0 <31724729+phutucthinh0@users.noreply.github.com>
2020-03-23 04:19:31 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,38 +1,8 @@
-var recognizing;
-  var recognition = new webkitSpeechRecognition();
-  recognition.continuous = true;
-  recognition.interimResults = true;
-  reset();
-  recognition.onend = reset;
+function translate(sourceLang, targetLang, sourceText) {
+    let url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="
+        + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
+    $.get(url, function (data) {
+        final_span_t.innerHTML = data[0][0][0];
+    });
+}
 
-  recognition.onresult = function (event) {
-    var final = "";
-    var interim = "";
-    for (var i = 0; i < event.results.length; ++i) {
-      if (event.results[i].isFinal) {
-        final += event.results[i][0].transcript;
-      } else {
-        interim += event.results[i][0].transcript;
-      }
-    }
-    final_span.innerHTML = final;
-    interim_span.innerHTML = interim;
-  }
-
-  function reset() {
-    recognizing = false;
-    button.innerHTML = "Click to Speak";
-  }
-
-  function toggleStartStop() {
-    if (recognizing) {
-      recognition.stop();
-      reset();
-    } else {
-      recognition.start();
-      recognizing = true;
-      button.innerHTML = "Click to Stop";
-      final_span.innerHTML = "";
-      interim_span.innerHTML = "";
-    }
-  }
