d8f64e7570dfa630d50d003e6ee2932f5b4b9bf1
phutucthinh0 <31724729+phutucthinh0@users.noreply.github.com>
2020-04-01 20:55:52 +0700

    fix

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 function translate(sourceLang, targetLang, sourceText) {
     let url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="
         + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
-    $.get(url, function (data) {
-        final_span_t.innerHTML = data[0][0][0];
-    });
+    return url;
 }
 
